7455629fb9f3146ccf915e4181d6005b41a7ac18
ashiq2002 <mdashiqhossain6@gmail.com>
2025-09-28 13:23:30 +0600

    updated

--- ashiq_project/__brick__/lib/core/config/strings/api_endpoint.dart
+++ ashiq_project/__brick__/lib/core/config/strings/api_endpoint.dart
@@ -2,5 +2,7 @@ class ApiEndpoint {
   static const String stagingBAseUrl = "localhost:3000/api/v1/";
   static const String productionBAseUrl = "https://api.example.com/v1/";
   static const String baseUrl = productionBAseUrl;
-    static const String imageBaseUrl = "";
+  static const String imageBaseUrl = "";
+
+  static const String login = "login";
 }

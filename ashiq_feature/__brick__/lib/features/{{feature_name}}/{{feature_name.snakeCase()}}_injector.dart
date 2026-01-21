f83eaec95ea2cc341a13c85c61e2a331ea5c2ed2
ashiq2002 <mdashiqhossain6@gmail.com>
2026-01-21 06:55:06 +0600

    getx logic updated

--- ashiq_feature/__brick__/lib/features/{{feature_name}}/{{feature_name.snakeCase()}}_injector.dart
+++ ashiq_feature/__brick__/lib/features/{{feature_name}}/{{feature_name.snakeCase()}}_injector.dart
@@ -1,13 +1,16 @@
+import 'package:get/get.dart';
 import '../../core/di/init_dependencies.dart';
 import '../../core/network/connection_checker.dart';
 import 'data/datasources/{{feature_name.snakeCase()}}_remote_data_source.dart';
 import 'data/repositories/{{feature_name.snakeCase()}}_repository_impl.dart';
 import 'domain/repositories/{{feature_name.snakeCase()}}_repository.dart';
 import 'domain/usecase/{{feature_name.snakeCase()}}_usecase.dart';
-
 {{#use_bloc}}
 import 'presentation/bloc/{{feature_name.snakeCase()}}_bloc.dart';
 {{/use_bloc}}
+{{#use_getx}}
+import 'presentation/controllers/{{feature_name.snakeCase()}}_controller.dart';
+{{/use_getx}}
 
 class {{feature_name.pascalCase()}}Injector {
   /// Initialize {{feature_name.pascalCase()}} feature dependencies
@@ -43,5 +46,12 @@ class {{feature_name.pascalCase()}}Injector {
       ),
     );
     {{/use_bloc}}
+
+    {{#use_getx}}
+    // GetX-controller
+    Get.lazyPut<{{feature_name.pascalCase()}}Controller>(
+      () => {{feature_name.pascalCase()}}Controller({{feature_name.snakeCase()}}UseCase: sl<{{feature_name.pascalCase()}}UseCase>()),
+    );
+    {{/use_getx}}
   }
 }

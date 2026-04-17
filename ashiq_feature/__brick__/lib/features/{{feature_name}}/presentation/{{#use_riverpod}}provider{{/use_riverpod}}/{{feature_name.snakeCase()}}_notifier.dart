21005c907b8531c842ccf7ddaad22536dbc1fce9
ashiq2002 <mdashiqhossain6@gmail.com>
2026-04-17 15:46:28 +0600

    riverpod fixed

--- ashiq_feature/__brick__/lib/features/{{feature_name}}/presentation/{{#use_riverpod}}provider{{/use_riverpod}}/{{feature_name.snakeCase()}}_notifier.dart
+++ ashiq_feature/__brick__/lib/features/{{feature_name}}/presentation/{{#use_riverpod}}provider{{/use_riverpod}}/{{feature_name.snakeCase()}}_notifier.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '{{feature_name.snakeCase()}}_state.dart';
 
-class {{feature_name.pascalCase()}}Notifier
-    extends Notifier<{{feature_name.pascalCase()}}State> {
+class {{feature_name.pascalCase()}}Notifier extends Notifier<{{feature_name.pascalCase()}}State> {
 
   @override
   {{feature_name.pascalCase()}}State build() {
@@ -9,7 +9,7 @@ class {{feature_name.pascalCase()}}Notifier
   }
 
   Future<void> load() async {
-    final usecase = ref.read(get{{feature_plural.pascalCase()}}Provider);
+    final usecase = ref.read(get{{feature_name.pascalCase()}}Provider);
 
     state = state.copyWith(isLoading: true, error: null);
 
